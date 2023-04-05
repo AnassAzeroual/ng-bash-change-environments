78aaecd52972d7f6a3d7f4e2de5036a5d3210fe6
Anass Azeroual <azeroual.anass2015@gmail.com>
2023-04-05 22:18:02 +0000

    update config completed

--- update_config.sh
+++ update_config.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-echo "Updating config.ts..."
+CONFIG_FILE="src/assets/config.ts"
+
+echo "Updating $CONFIG_FILE..."
 
 # Read the keys in the config.ts file and create a sed command to set all values to empty string
 SED_COMMAND=""
 while read -r line; do
   VAR_NAME=$(echo "$line" | grep -Eo '^[^:]+')
   SED_COMMAND+="s/\b$VAR_NAME:\s*[^,}]*[,}]/$VAR_NAME: \"\",/g;"
-done < <(grep -Eo '^[^:]+:' src/assets/config.ts)
+done < <(grep -Eo '^[^:]+:' "$CONFIG_FILE")
 
 # Execute the sed command to update the config.ts file
-sed -i "$SED_COMMAND" src/assets/config.ts
+sed -i "$SED_COMMAND" "$CONFIG_FILE"
 
-echo "Empty values updated in config.ts."
+echo "Empty values updated in $CONFIG_FILE."
 
 # Get the names of all the variables defined in config.ts
-VARIABLES=$(grep -Eo '^ *\w+:' src/assets/config.ts | sed 's/://')
+VARIABLES=$(grep -Eo '^ *\w+:' "$CONFIG_FILE" | sed 's/://')
 
 # Loop through each variable and create a sed command to update matching keys
 SED_COMMAND=""
@@ -30,7 +32,7 @@ for VAR_NAME in $VARIABLES; do
 done
 
 # Execute the sed command to update the config.ts file
-echo "Running sed command: sed -i \"$SED_COMMAND\" src/assets/config.ts"
-sed -i "$SED_COMMAND" src/assets/config.ts
+echo "Running sed command: sed -i \"$SED_COMMAND\" $CONFIG_FILE"
+sed -i "$SED_COMMAND" "$CONFIG_FILE"
 
-echo "Config.ts updated."
+echo "$CONFIG_FILE updated."
