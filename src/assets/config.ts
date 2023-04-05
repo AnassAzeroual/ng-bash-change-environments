1d24dd199de56163166657474f945cc07a5d3f98
Anass Azeroual <azeroual.anass2015@gmail.com>
2023-04-05 18:35:44 +0000

    work's

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,4 +1,6 @@
 export const config = {
-    Angular_v: "160",
-    Ionic_v: "70"
+    Angular_v: "15",
+    Ionic_v: "6",
+    Css_v: "30",
+    Html_v: "60",
 }
--- update_config.sh
+++ update_config.sh
@@ -2,15 +2,21 @@
 
 echo "Updating config.ts..."
 
-# Read the environment variables and create a sed command to update matching keys
+# Get the names of all the variables defined in config.ts
+VARIABLES=$(grep -Eo '^ *\w+:' src/assets/config.ts | sed 's/://')
+
+# Loop through each variable and create a sed command to update matching keys
 SED_COMMAND=""
-while read -r line; do
-  VAR_NAME=$(echo "$line" | cut -d= -f1)
-  VAR_VALUE=$(echo "$line" | cut -d= -f2-)
-  SED_COMMAND+="s/\b$VAR_NAME:\s*\"[^\"]*\"/$VAR_NAME: \"$VAR_VALUE\"/g;"
-done < <(env | grep -E "^Angular_v=|^Ionic_v=")
+for VAR_NAME in $VARIABLES; do
+  VAR_VALUE=$(echo "${!VAR_NAME}")
+  if [ -n "$VAR_VALUE" ]; then
+    SED_COMMAND+="s/\b$VAR_NAME:\s*\"[^\"]*\"/$VAR_NAME: \"$VAR_VALUE\"/g;"
+    echo "Found environment variable for $VAR_NAME: $VAR_VALUE"
+  fi
+done
 
 # Execute the sed command to update the config.ts file
+echo "Running sed command: sed -i \"$SED_COMMAND\" src/assets/config.ts"
 sed -i "$SED_COMMAND" src/assets/config.ts
 
 echo "Config.ts updated."
