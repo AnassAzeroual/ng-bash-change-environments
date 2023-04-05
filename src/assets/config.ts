2a4a946ceceefc2b88ed1f7c700045e278c8cd7d
Anass Azeroual <azeroual.anass2015@gmail.com>
2023-04-05 22:14:29 +0000

    update config complated

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,8 +1,5 @@
 // this is config.ts file
 export const config = {
-    Angular_v: "",
-    Ionic_v: "",
-    Css_v: "",
-    Html_v: "",
-    Html_vs: "",
+    Angular_v: "15",
+    Ionic_v: "6",
 }
--- update_config.sh
+++ update_config.sh
@@ -2,6 +2,18 @@
 
 echo "Updating config.ts..."
 
+# Read the keys in the config.ts file and create a sed command to set all values to empty string
+SED_COMMAND=""
+while read -r line; do
+  VAR_NAME=$(echo "$line" | grep -Eo '^[^:]+')
+  SED_COMMAND+="s/\b$VAR_NAME:\s*[^,}]*[,}]/$VAR_NAME: \"\",/g;"
+done < <(grep -Eo '^[^:]+:' src/assets/config.ts)
+
+# Execute the sed command to update the config.ts file
+sed -i "$SED_COMMAND" src/assets/config.ts
+
+echo "Empty values updated in config.ts."
+
 # Get the names of all the variables defined in config.ts
 VARIABLES=$(grep -Eo '^ *\w+:' src/assets/config.ts | sed 's/://')
 
@@ -12,6 +24,8 @@ for VAR_NAME in $VARIABLES; do
   if [ -n "$VAR_VALUE" ]; then
     SED_COMMAND+="s/\b$VAR_NAME:\s*\"[^\"]*\"/$VAR_NAME: \"$VAR_VALUE\"/g;"
     echo "Found environment variable for $VAR_NAME: $VAR_VALUE"
+  else
+    SED_COMMAND+="s/\b$VAR_NAME:\s*[^,}]*[,}]/$VAR_NAME: \"\",/g;"
   fi
 done
 
